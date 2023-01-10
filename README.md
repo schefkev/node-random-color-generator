5b8d719aa6f870d5404eb8ca97f2a0493c442cd2
Kevin Toth <kevin.toth16@gmail.com>
2023-01-10 16:45:51 +0100

    First Working Project

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Node.js Random Color Generator
+
+## Options
+
+You can pass an options object to influence the type of color it produces. The options object accepts the following properties:
+
+`hue` - Controls the hue of the generated color. You can pass a string representing a color name:
+
+`red`
+`orange`
+`yellow`
+`green`
+`blue`
+`purple`
+`pink`
+
+`luminosity` - Controls the luminosity of the generated color. You can specify a string containing:
+
+`bright`
+`light`
+`dark`
---
+++ index.js
@@ -0,0 +1,36 @@
+import chalk from 'chalk';
+import color from 'randomcolor';
+
+const hueInput = process.argv[2];
+const lumInput = process.argv[3]
+
+const ranColor = color.randomColor({
+  luminosity: lumInput,
+  hue: hueInput
+});
+
+function logRandomColor(str){
+  const log = console.log;
+  log(chalk.hex(ranColor).bold(str));
+}
+
+
+
+// Stretch goals
+/*
+  If a user types 'ask' instead of a color name, print questions to ask the user for the name of the color and the luminosity.
+*/
+
+if (hueInput === 'ask') {
+  console.log('Which color and luminosity you wanna use?');
+} else {
+  logRandomColor(`###############################
+###############################
+###############################
+####                       ####
+####      ${ranColor}      ####
+####                       ####
+###############################
+###############################
+###############################`);
+}
